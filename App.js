93c632c32e5c9191fff2fd68a01b6b678ad7d2b8
Mads Frank <madsf88@gmail.com>
2018-05-28 12:53:25 +0200

    Mere merge

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import plan from './data/training-plan.json';
 
 export default class App extends React.Component {
   render() {
